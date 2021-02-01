d4fbde832f81243111e48ee397fa298786ac6a24
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2021-02-01 14:44:22 +0100

    Bump v0.1.1
    * Remove duplicate Norwegian language
    * Remove translation file for the source language
    * Update Foris JS library to version 5.1.9

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ class HaasBuild(build_py):
 
 setuptools.setup(
     name=NAME,
-    version='0.1.0',
+    version='0.1.1',
     packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
 
