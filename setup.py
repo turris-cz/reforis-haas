b42feb0f297d03b737bec36dfe41b00b3199d786
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-01-30 20:27:24 +0300

    Drop pylint-quotes in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#  Copyright (C) 2019-2022 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+#  Copyright (C) 2019-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
 #
 #  This is free software, licensed under the GNU General Public License v3.
 #  See /LICENSE for more information.
@@ -49,7 +49,6 @@ setuptools.setup(
         'devel': [
             'pytest',
             'pylint',
-            'pylint-quotes',
             'pycodestyle',
             'reforis @ git+https://gitlab.nic.cz/turris/reforis/reforis#egg=reforis',
         ],
