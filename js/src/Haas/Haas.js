a229e863e34c883a7f5ce58380274cd23ce992cb
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2024-09-26 14:33:55 +0200

    Migrate to FontAwesome v6

--- js/src/Haas/Haas.js
+++ js/src/Haas/Haas.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2022 CZ.NIC z.s.p.o. (https://www.nic.cz/)
+ * Copyright (C) 2020-2024 CZ.NIC z.s.p.o. (https://www.nic.cz/)
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See /LICENSE for more information.
@@ -7,6 +7,8 @@
 
 import React from "react";
 
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ForisForm } from "foris";
 
 import HaasForm from "./HaasForm";
@@ -21,13 +23,25 @@ export default function Haas() {
                     "Honeypot as a Service (HaaS) is a special software which simulates an operating system and allows an attacker to log in via SSH or telnet and execute commands or download malware."
                 )}
             </p>
-            <p
-                dangerouslySetInnerHTML={{
-                    __html: _(
-                        `If you want to use Honeypot as a Service on your router, you should have the public IPv4 address, register on the HaaS website, and fill out the token. Also, there you can see sessions and statistics from the SSH honeypot from your router. For more information click <a href="https://haas.nic.cz/" target="_blank" rel="noopener noreferrer">here<sup><i class="fas fa-external-link-alt link-outside-icon fa-xs ml-1"></i></sup></a>.`
-                    ),
-                }}
-            />
+            <p>
+                {_(
+                    "If you want to use Honeypot as a Service on your router, you should have the public IPv4 address, register on the HaaS website, and fill out the token. Also, there you can see sessions and statistics from the SSH honeypot from your router. For more information click "
+                )}
+                <a
+                    href="https://haas.nic.cz/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {_("here")}
+                    <sup>
+                        <FontAwesomeIcon
+                            icon={faExternalLinkAlt}
+                            className="fa-xs ms-1"
+                        />
+                    </sup>
+                </a>
+                .
+            </p>
             <ForisForm
                 forisConfig={{
                     endpoint: API_URLs.haas,
